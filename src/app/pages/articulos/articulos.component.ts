5c5a19bf7fa9a8df6f88dccda9ace0cf805646e4
JOSE LUIS GARCIA CRUZ <Z041233@altecmexico.mx.bsch>
2019-03-05 16:21:30 -0600

    inicio

---
+++ src/app/pages/articulos/articulos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'articulos',
+  templateUrl: './articulos.component.html',
+  styleUrls: ['./articulos.component.css']
+})
+export class ArticulosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
